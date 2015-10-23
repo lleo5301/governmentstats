40f404d3cd7ac7a41966c7db20fdf918be8943ab
leonardo Quintero <lleo5301@gmail.com>
2015-10-23 06:31:31 -0400

    move data files

--- sync-data.js
+++ sync-data.js
@@ -6,36 +6,39 @@ var async = require('async');
 mongoose.connect(db.db_url);
 
 //import models
-var Unemployment = require('./models/Unemployement');
+var Unemployment = require('./models/unemployement');
 
 //retrieve file for parsing
 var u_file = require(db.unemployement_file);
 
 //placeholder for our columns
-var columns = [];
+var columns = []; 
+
+var allData = []
 
 //function to add unemployment record to db
 var addUnenployment = function(item,callback){
-	var new_unemployement = {};
-	item.map(function(column,i){
-		new_unemployement[columns[i]] = column;
-	})
+	
+	
 	// console.log(new_unemployement);
 	//add to db
 	// new_unemployement = new Unemployment(new_unemployement);
-	var newU = new Unemployment(new_unemployement)
+	var newU = new Unemployment(item)
+	// allData.push(newU);
+
 	// console.log(new_unemployement);
 	newU.save(function(err, new_unemployement){
 		console.log('this happened');
-		// if(err){
-		// 	console.log(err.message);
-		// 	callback(err);
-		// }else{
-		// 	console.log('record Added');
-		// 	callback();
-		// }
+		if(err){
+			console.log(err.message);
+			// callback(err);
+		}else{
+			console.log('record Added');
+			// callback();
+		}
 		callback();
 	})
+	
 
 }
 
@@ -43,8 +46,13 @@ var addUnenployment = function(item,callback){
 
 //handle error
 var err_callback = function(e){
-	console.log(e);
+	// console.log(e);
+	console.log(allData);
 }
+// newU = new Unemployment();
+// newU.save(function(err, data){
+// 	console.log(err, data); 
+// })
 
 //function to parse data
 //recieves a file
@@ -54,7 +62,34 @@ var parse = function(data){
 	//get columns to build object
 	columns = get_columns(data.meta.view.columns);
     var rows = data.data;
-    async.each(rows,addUnenployment,err_callback);
+    var all_data = [];
+    rows.map(function(item){
+    	var new_unemployement = {};
+    	 item.map(function(column,i){
+			new_unemployement[columns[i]] = column;
+		});
+    	all_data.push(new_unemployement);
+    })
+    console.log(all_data.length);
+   
+    async.each(all_data,function(item,callback){
+	
+	console.log('running add');
+	// console.log(new_unemployement);
+	//add to db
+	// new_unemployement = new Unemployment(new_unemployement);
+	var newU = new Unemployment(item)
+	// allData.push(newU);
+
+	// console.log(new_unemployement);
+	newU.save(function(err, new_unemployement){
+		console.log('this happened');
+		console.log(err, new_unemployement);
+		callback();
+	})
+	
+
+},err_callback);
     
 
 }
