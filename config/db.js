3592f7cc163474fa70e1398fe7afe17a54960e0f
leonardo Quintero <lleo5301@gmail.com>
2015-10-27 07:23:13 -0400

    Additional fields

--- config/db.js
+++ config/db.js
@@ -1,5 +1,12 @@
 module.exports = {
+	fileLocation:'./data/',
 	db_url:'mongodb://localhost:27017/government',
 	unemployement_file: './unenployment.json',
+	files:[{
+		name:'consumer_complaints.csv',
+		data:'ConsumerComplaint',
+		delimeter:',',
+		model:'./models/ConsumerComplaint.js'
+	}]
 
 };
